d95f61b0d7c03c8fd74ee2fd006c650652f05ff2
amwatah <jackalltrade2020@gmail.com>
2022-06-17 17:27:46 +0300

    added firebase

--- src/routes/create/fundraise.svelte
+++ src/routes/create/fundraise.svelte
@@ -1,16 +1,64 @@
 <script>
+	import { db } from '$lib/firebase/config';
+	import { addDoc, collection } from 'firebase/firestore';
+	let fundTitle = '';
+	let fundAppeal = '';
+	let fundAmount = '';
+	let fundContacts = '';
+	let fundBilling = ' ';
 
+	async function submitFundraiser() {
+		await addDoc(collection(db, 'fundraisers'), {
+			title: fundTitle,
+			appeal: fundAppeal,
+			amount: fundAmount,
+			conctacts: fundContacts,
+			billing: fundBilling
+		})
+			.then(() => {
+				alert('Submitted sucessfully ');
+			})
+			.catch((e) => alert(e));
+	}
 </script>
-<div class="page grid grid-cols-12 pt-[5vh] pb-[10vh]">
-    <h1 class=" col-span-12 text-center"> Fundraise  </h1>
-    <input type="text" placeholder="Title" class="input input-primary col-span-12 m-2 rounded-none">
-    <textarea placeholder="Appeal" name="" id=""  cols="30" rows="5" class=" textarea textarea-primary col-span-12 m-2 rounded-none"></textarea>
-    <input type="text" placeholder="Target Amount" class="input input-primary col-span-12 m-2 rounded-none">
-    <input type="text" placeholder="Contact" class="input input-primary col-span-6 m-2 rounded-none">
-    <input type="text" placeholder="Billing address" class="input input-primary col-span-6 m-2 rounded-none">
-     
-    <button class="btn  btn-primary col-span-12  m-2 rounded-none ">Submit</button>
-    
-     
 
+<div class="page grid grid-cols-12 pt-[5vh] pb-[10vh]">
+	<h1 class=" col-span-12 text-center">Fundraise</h1>
+	<input
+		bind:value={fundTitle}
+		type="text"
+		placeholder="Title"
+		class="input input-primary col-span-12 m-2 rounded-none"
+	/>
+	<textarea
+		bind:value={fundAppeal}
+		placeholder="Appeal"
+		name=""
+		id=""
+		cols="30"
+		rows="5"
+		class=" textarea textarea-primary col-span-12 m-2 rounded-none"
+	/>
+	<input
+		bind:value={fundAmount}
+		type="text"
+		placeholder="Target Amount"
+		class="input input-primary col-span-12 m-2 rounded-none"
+	/>
+	<input
+		bind:value={fundContacts}
+		type="text"
+		placeholder="Contact"
+		class="input input-primary col-span-6 m-2 rounded-none"
+	/>
+	<input
+		bind:value={fundBilling}
+		type="text"
+		placeholder="Billing address"
+		class="input input-primary col-span-6 m-2 rounded-none"
+	/>
+
+	<button on:click={submitFundraiser} class="btn  btn-primary col-span-12  m-2 rounded-none "
+		>Submit</button
+	>
 </div>
--- src/routes/create/meeting.svelte
+++ src/routes/create/meeting.svelte
@@ -1,17 +1,65 @@
 <script>
+	import { db } from '$lib/firebase/config';
+	import { addDoc, collection } from 'firebase/firestore';
+	let meetingTitle = '';
+	let meetingLocation = '';
+	let meetingParticipants = '';
+	let meetingDescribtion = '';
+	let meetingDate = ' ';
 
+	async function submitMeeting() {
+		await addDoc(collection(db, 'meetings'), {
+			title: meetingTitle.toUpperCase(),
+			location: meetingLocation.toLocaleUpperCase(),
+			participants: meetingParticipants.toUpperCase(),
+			describtion: meetingDescribtion,
+			date: meetingDate
+		})
+			.then(() => {
+				alert('Submitted sucessfully ');
+			})
+			.catch((e) => alert(e));
+	}
 </script>
-<div class="page grid grid-cols-12 pt-[5vh]">
-    <h1 class=" col-span-12 text-center">New Meetup</h1>
-    <input type="text" placeholder="Title" class="input input-primary col-span-12 m-2 rounded-none">
-    <input type="text" placeholder="location" class="input input-primary col-span-12 m-2 rounded-none">
-    <input type="text" placeholder="participants" class="input input-primary col-span-12 m-2 rounded-none"> 
-     
-    
-    <textarea placeholder="Describtion" name="" id=""  cols="30" rows="5" class=" textarea textarea-primary col-span-12 m-2 rounded-none"></textarea>
-    <input type="datetime-local"  class="col-span-12 text-center p-2 border-2 border-primary m-2" name="" id="">
-    <button class="btn  btn-primary col-span-12  m-2 rounded-none ">Submit</button>
-    
-     
 
+<div class="page grid grid-cols-12 pt-[5vh]">
+	<h1 class=" col-span-12 text-center">New Meetup</h1>
+	<input
+		bind:value={meetingTitle}
+		type="text"
+		placeholder="Title"
+		class="input input-primary col-span-12 m-2 rounded-none"
+	/>
+	<input
+		bind:value={meetingLocation}
+		type="text"
+		placeholder="location"
+		class="input input-primary col-span-12 m-2 rounded-none"
+	/>
+	<input
+		bind:value={meetingParticipants}
+		type="text"
+		placeholder="participants"
+		class="input input-primary col-span-12 m-2 rounded-none"
+	/>
+
+	<textarea
+		bind:value={meetingDescribtion}
+		placeholder="Describtion"
+		name=""
+		id=""
+		cols="30"
+		rows="5"
+		class=" textarea textarea-primary col-span-12 m-2 rounded-none"
+	/>
+	<input
+		bind:value={meetingDate}
+		type="datetime-local"
+		class="col-span-12 text-center p-2 border-2 border-primary m-2"
+		name=""
+		id=""
+	/>
+	<button on:click={submitMeeting} class="btn  btn-primary col-span-12  m-2 rounded-none "
+		>Submit</button
+	>
 </div>
