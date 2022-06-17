13c1bbad1b25ed01c84e066ac9f11a4ce873cd6c
amwatah <jackalltrade2020@gmail.com>
2022-06-17 15:54:16 +0300

    intial commit

---
+++ src/routes/create/announce.svelte
@@ -0,0 +1,31 @@
+<script>
+	let announcementTitle = '';
+	let announcementDescribion = '';
+
+	function submitAnouncement() {
+
+        
+    }
+</script>
+
+<div class="page grid grid-cols-12 pt-[5vh]">
+	<h1 class=" col-span-12 text-center ">New Announcement</h1>
+	<input
+		bind:value={announcementTitle}
+		type="text"
+		placeholder="Title"
+		class="input input-primary col-span-12 m-2 rounded-none"
+	/>
+	<textarea
+		bind:value={announcementDescribion}
+		placeholder="Describtion"
+		name=""
+		id=""
+		cols="30"
+		rows="5"
+		class=" textarea textarea-primary col-span-12 m-2 rounded-none"
+	/>
+	<button on:click={submitAnouncement} class="btn  btn-primary col-span-12  m-2 rounded-none "
+		>Submit</button
+	>
+</div>
