38ad52c3a3d589291b096836246e0ddbaa04c4c9
amwatah <jackalltrade2020@gmail.com>
2022-06-29 11:08:22 +0300

    is alpha the first release ?

--- src/routes/create/announce.svelte
+++ src/routes/create/announce.svelte
@@ -1,49 +1,50 @@
 <script>
 	import { db } from '$lib/firebase/config';
-import { loggedIn } from '$lib/stores/globals';
-	import { addDoc, collection } from 'firebase/firestore';
+	import { loggedIn, userDetails } from '$lib/stores/globals';
+	import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+
+	
 	let announcementTitle = '';
 	let announcementDescribion = '';
 
 	async function submitAnouncement() {
 		await addDoc(collection(db, 'announcements'), {
 			title: announcementTitle.toUpperCase(),
-			describtion: announcementDescribion
-		}).then(()=>{
-             alert(
-                "Submitted sucessfully "
-             )
-
-        })
+			describtion: announcementDescribion,
+			createdBy: $userDetails,
+			upvotes: 0,
+			created: serverTimestamp()
+		}).then(() => {
+			alert('Submitted sucessfully ');
+		});
 	}
 </script>
 
 <div class="page grid grid-cols-12 pt-[5vh]">
-	{#if $loggedIn === false }
-	<section class='w-screen h-screen flex flex-col items-center justify-center text-error'>
-		<h1 class= " font-bold text-xl"> LOGGIN TO CREATE  OR VIEW CONTENT !!  </h1>
-		<a href="/" class=' btn btn-outline btn-primary'>LOG IN </a>
-	</section>
+	{#if $loggedIn === false}
+		<section class="w-screen h-screen flex flex-col items-center justify-center text-error">
+			<h1 class=" font-bold text-xl">LOGGIN TO CREATE OR VIEW CONTENT !!</h1>
+			<a href="/" class=" btn btn-outline btn-primary">LOG IN </a>
+		</section>
 	{:else}
-	<h1 class=" col-span-12 text-center ">New Announcement</h1>
-	<input
-		bind:value={announcementTitle}
-		type="text"
-		placeholder="Title"
-		class="input input-primary col-span-12 m-2 rounded-none"
-	/>
-	<textarea
-		bind:value={announcementDescribion}
-		placeholder="Describtion"
-		name=""
-		id=""
-		cols="30"
-		rows="5"
-		class=" textarea textarea-primary col-span-12 m-2 rounded-none"
-	/>
-	<button on:click={submitAnouncement} class="btn  btn-primary col-span-12  m-2 rounded-none "
-		>Submit</button
-	>
+		<h1 class=" col-span-12 text-center ">New Announcement</h1>
+		<input
+			bind:value={announcementTitle}
+			type="text"
+			placeholder="Title"
+			class="input input-primary col-span-12 m-2 rounded-none"
+		/>
+		<textarea
+			bind:value={announcementDescribion}
+			placeholder="Describtion"
+			name=""
+			id=""
+			cols="30"
+			rows="5"
+			class=" textarea textarea-primary col-span-12 m-2 rounded-none"
+		/>
+		<button on:click={submitAnouncement} class="btn  btn-primary col-span-12  m-2 rounded-none "
+			>Submit</button
+		>
 	{/if}
-	
 </div>
